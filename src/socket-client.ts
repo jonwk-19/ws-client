a21d5ec765c4fa2d42663d4887988c68607f4aeb
unknown <jonwk19@outlook.com>
2025-08-06 17:03:48 -0600

    NO-JIRA: Everiting is done

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,5 +1,7 @@
 import { Manager, Socket } from 'socket.io-client'
 
+let socket: Socket | undefined;
+
 export const connectToSeerver = ( token: string) => {
 
   const manager = new Manager('http://localhost:3000/socket.io/socket.io.js', {
@@ -9,14 +11,15 @@ export const connectToSeerver = ( token: string) => {
     }
   })
 
-  const socket = manager.socket('/')
+  socket?.removeAllListeners();
+  socket = manager.socket('/')
 
   console.log({socket})
-  addListeners(socket)
+  addListeners()
 }
 
 
-const addListeners = (socket: Socket) => {
+const addListeners = () => {
   const messageForm = document.querySelector<HTMLFormElement>('#message-form')!;
   const messageInput = document.querySelector<HTMLInputElement>('#message-input')!;
   const messagesUl = document.querySelector<HTMLUListElement>('#messages-ul')!;
