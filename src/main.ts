f6323c03cda6d5e849bf345fdac1333fa1f9201b
unknown <jonwk19@outlook.com>
2025-08-06 13:46:15 -0600

    NO-JIRA: Add listen server to check how many clients are connected

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>WebSocket - Client</h1>
     <span id='server-status'>offline</span>
+
+    <ul id="clients-ul">
+    </ul>
   </div>
 `
 
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -13,6 +13,9 @@ export const connectToSeerver = () => {
 
 const addListeners = (socket: Socket) => {
   const serverStatusLabel = document.querySelector<HTMLSpanElement>('#server-status')!;
+  // Todo: #clients-ul
+
+
   socket.on('connect', () => {
     console.log('Connected to server');
     serverStatusLabel.textContent = 'online';
@@ -21,6 +24,16 @@ const addListeners = (socket: Socket) => {
   socket.on('disconnect', () => {
     console.log('Disconnected from server');
     serverStatusLabel.textContent = 'offline';
+  });
+
+  socket.on('clients-updated', (clients: string[]) => {
+    const clientsUl = document.querySelector<HTMLUListElement>('#clients-ul')!;
+    clientsUl.innerHTML = '';
+    clients.forEach(client => {
+      const li = document.createElement('li');
+      li.textContent = client;
+      clientsUl.appendChild(li);
+    });
   });
 
 }
